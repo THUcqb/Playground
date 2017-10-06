de635f95539220902ec403e761a14d5b10d1898e
THUcqb <chenqibin422@gmail.com>
2017-10-06 21:11:40 +0800

    feat: Add navigation bar with material-ui
    Arrange the layout and add a navigation bar and drawer.
    Add new dependency material-ui and use font roboto.
    Downgraded react to 15.0.0.
    
    fix: #6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Operation from './Operation';
 import Game from './Game';
+import Navigation from './Navigation'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 
 class App extends Component
@@ -8,13 +10,17 @@ class App extends Component
     render()
     {
         return (
+            <MuiThemeProvider>
             <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Snake</h1>
-                </header>
-                <Operation className="Operation"/>
-                <Game className="Game"/>
+                <Navigation className="App-header"/>
+                <div className='SceneArea'>
+                    <Game className="Game"/>
+                </div>
+                <div className='GamepadArea'>
+                    <Operation className="Operation"/>
+                </div>
             </div>
+            </MuiThemeProvider>
         );
     }
 }
