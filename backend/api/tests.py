d814ff6ade3b5b74660326c714fcef36776ff603
何杰 <hejie15@mails.tsinghua.edu.cn>
2017-10-18 20:05:55 +0800

    feat:Implement immanent maps' storing and loading Resolves:#34

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -3,11 +3,10 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 import unittest
-from api.models import UserInfo
+from api.models import UserInfo, ImmanentMaps
 from django.test import Client
 import json
 
-# Create your tests here.
 class UserTestCase(TestCase):
     def setUp(self):
         pass
@@ -18,12 +17,17 @@ class ModelTest(TestCase):
     def setUp(self):
         UserInfo.objects.create(username = 'First', password = 'first', phonenumber = '110', email = 'first@example.com')
         UserInfo.objects.create(username = 'Second', password = 'second', phonenumber = '110', email = 'second@example.com')
+        ImmanentMaps.objects.create(level = '1', immanentmap = '1111000000100002000010010000211000020001000000000110200000000000000200020010000000002001010000000021')
     
-    def test_str(self):
+    def test_str_1(self):
         First = UserInfo.objects.get(username = 'First')
         Second = UserInfo.objects.get(username = 'Second')
         self.assertEqual(First.__str__(), 'First')
         self.assertEqual(Second.__str__(), 'Second')
+        
+    def test_str_2(self):
+        map_1 = ImmanentMaps.objects.get(level = '1')
+        self.assertEqual(map_1.__str__(), '1')
 
 class UsersystemTest(TestCase):
     def setUp(self):
@@ -88,3 +92,32 @@ class UsersystemTest(TestCase):
         get_res_3 = self.client.post(get_url, get_json_data_3, content_type = 'application/json')
         get_text_3 = json.loads(get_res_3.content.decode('utf-8'))
         self.assertEqual(get_text_3['status'], 'failed')
+
+class ImmanentmapsTestcase(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_savemaps(self):
+        save_url = '/maps/save'
+        parameter = {'level':'1', 'maps':'1111000000100002000010010000211000020001000000000110200000000000000200020010000000002001010000000021'}
+        json_parameter = json.dumps(parameter)
+        res_1 = self.client.post(save_url, json_parameter, content_type = "application/json")
+        text_1 = json.loads(res_1.content.decode('utf-8'))
+        self.assertEqual(text_1['status'], 'saved')
+        res_2 = self.client.post(save_url, json_parameter, content_type = "application/json")
+        text_2 = json.loads(res_2.content.decode('utf-8'))
+        self.assertEqual(text_2['status'], 'existed')
+    
+    def test_loadmaps(self):
+        load_url = '/maps/load'
+        ImmanentMaps.objects.create(level = '1', immanentmap = '1111000000100002000010010000211000020001000000000110200000000000000200020010000000002001010000000021')
+        parameter_1 = {'level':'1'}
+        json_parameter_1 = json.dumps(parameter_1)
+        parameter_2 = {'level':'3'}
+        json_parameter_2 = json.dumps(parameter_2)
+        res_1 = self.client.post(load_url, json_parameter_1, content_type = "application/json")
+        text_1 = json.loads(res_1.content.decode('utf-8'))
+        self.assertEqual(text_1['status'], 'successful')
+        res_2 = self.client.post(load_url, json_parameter_2, content_type = "application/json")
+        text_2 = json.loads(res_2.content.decode('utf-8'))
+        self.assertEqual(text_2['status'], 'doesnotexist')
--- backend/api/usersystem.py
+++ backend/api/usersystem.py
@@ -12,88 +12,84 @@ import time
 
 @csrf_exempt
 def register(request):
-	res = {}
-	#if request.method == 'GET':
-	#	return render_to_response('register.html')
-	if request.method == 'POST':
-		d = json.loads(request.body.decode('utf-8'))
-		response_data = {}
-		username = d['username']
-		password = d['password']
-		phonenumber = d['phonenumber']
-		email = d['email']
+    res = {}
+    if request.method == 'POST':
+        d = json.loads(request.body.decode('utf-8'))
+        response_data = {}
+        username = d['username']
+        password = d['password']
+        phonenumber = d['phonenumber']
+        email = d['email']
 
-		try:
-			exist_user = UserInfo.objects.get(username = username)
-		except UserInfo.DoesNotExist:
-			userinfo = UserInfo.objects.create(username = username, password = password, phonenumber = phonenumber, email = email)
-			response_data["status"] = "successful"
-			return HttpResponse(json.dumps(response_data),content_type="application/json")
+        try:
+            exist_user = UserInfo.objects.get(username = username)
+        except UserInfo.DoesNotExist:
+            userinfo = UserInfo.objects.create(username = username, password = password, phonenumber = phonenumber, email = email)
+            response_data["status"] = "successful"
+            return HttpResponse(json.dumps(response_data),content_type="application/json")
 
-		response_data["status"] = "failed"
-		return HttpResponse(json.dumps(response_data),content_type="application/json")
+        response_data["status"] = "failed"
+        return HttpResponse(json.dumps(response_data),content_type="application/json")
 
 @csrf_exempt
 def login(request):
-	res = {}
-	#if request.method == 'GET':
-	#	return render_to_response('login.html')
-	if request.method == 'POST':
-		d = json.loads(request.body.decode('utf-8'))
-		response_data = {}
-		try:
-			userinfo = UserInfo.objects.get(username = d['username'])
-			if userinfo.password == d['password']:
-				issuetime = time.time()
-				expiretime = issuetime + 604800
-				payload_dict = {
-					'iat':issuetime,
-					'exp':expiretime,
-					'iss':'admin',
-					'username':userinfo.username
-				}
-				payload_str = json.dumps(payload_dict)
-				payload = base64.b64encode(payload_str.encode(encoding = "utf-8"))
-				response_data["token"] = payload.decode()
-				response_data["status"] = "successful"
-				return HttpResponse(json.dumps(response_data),content_type="application/json")
-		except UserInfo.DoesNotExist:
-			pass
-		response_data["status"] = "failed"
-		return HttpResponse(json.dumps(response_data),content_type="application/json")
+    res = {}
+    if request.method == 'POST':
+        d = json.loads(request.body.decode('utf-8'))
+        response_data = {}
+        try:
+            userinfo = UserInfo.objects.get(username = d['username'])
+            if userinfo.password == d['password']:
+                issuetime = time.time()
+                expiretime = issuetime + 604800
+                payload_dict = {
+                    'iat':issuetime,
+                    'exp':expiretime,
+                    'iss':'admin',
+                    'username':userinfo.username
+                }
+                payload_str = json.dumps(payload_dict)
+                payload = base64.b64encode(payload_str.encode(encoding = "utf-8"))
+                response_data["token"] = payload.decode()
+                response_data["status"] = "successful"
+                return HttpResponse(json.dumps(response_data),content_type="application/json")
+        except UserInfo.DoesNotExist:
+            pass
+        response_data["status"] = "failed"
+        return HttpResponse(json.dumps(response_data),content_type="application/json")
 
 @csrf_exempt
 def logout(request):
-	if request.method == 'POST':
-		response_data = {}
-		response_data["status"] = "successful"
-		return HttpResponse(json.dumps(response_data),content_type="application/json")
+    if request.method == 'POST':
+        response_data = {}
+        response_data["status"] = "successful"
+        return HttpResponse(json.dumps(response_data),content_type="application/json")
 
 @csrf_exempt
 def getuserinfo(request):
-	if request.method == 'POST':
-		d = json.loads(request.body.decode('utf-8'))
-		token_byte = d['token']
-		token_str = token_byte.encode(encoding = "utf-8")
-		token_info = base64.b64decode(token_str)
-		token = token_info.decode('utf-8','ignore')
-		user_info = json.loads(token)
-		username = user_info['username']
-		response_data = {}
-		now = time.time()
-		expire = user_info['exp']
-		if expire < now:
-			response_data["status"] = "failed"
-			return HttpResponse(json.dumps(response_data),content_type="application/json")
-		try:
-			userinfo = UserInfo.objects.get(username = username)
-			response_data['username'] = userinfo.username
-			response_data['phonenumber'] = userinfo.phonenumber
-			response_data['email'] = userinfo.email
-			response_data['status'] = 'successful'
-			return HttpResponse(json.dumps(response_data),content_type="application/json")
-		except UserInfo.DoesNotExist:
-			pass
+    if request.method == 'POST':
+        d = json.loads(request.body.decode('utf-8'))
+        token_byte = d['token']
+        token_str = token_byte.encode(encoding = "utf-8")
+        token_info = base64.b64decode(token_str)
+        token = token_info.decode('utf-8','ignore')
+        user_info = json.loads(token)
+        username = user_info['username']
+        response_data = {}
+        now = time.time()
+        expire = user_info['exp']
+        if expire < now:
+            response_data["status"] = "failed"
+            return HttpResponse(json.dumps(response_data),content_type="application/json")
+        try:
+            userinfo = UserInfo.objects.get(username = username)
+            response_data['username'] = userinfo.username
+            response_data['phonenumber'] = userinfo.phonenumber
+            response_data['email'] = userinfo.email
+            response_data['status'] = 'successful'
+            return HttpResponse(json.dumps(response_data),content_type="application/json")
+        except UserInfo.DoesNotExist:
+            pass
 
-		response_data["status"] = "failed"
-		return HttpResponse(json.dumps(response_data),content_type="application/json")
+        response_data["status"] = "failed"
+        return HttpResponse(json.dumps(response_data),content_type="application/json")
