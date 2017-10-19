2a2a9caad83bac7595c24c71fd127b6e9cc3c551
左浩佳 <zuohj15@mails.tsinghua.edu.cn>
2017-10-19 15:40:50 +0800

    feat: Added preload-js
    relate #32

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import createjs from 'preload-js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let manifest;
+let preload;
+
+function setupManifest()
+{
+    manifest = [{
+        src: "logo.svg",
+        id: "logo"
+    }
+    ];
+}
+
+function startPreload()
+{
+    preload = new createjs.LoadQueue(false);
+    preload.on("progress", handleFileProgress);
+    preload.on("complete", loadComplete);
+    preload.on("error", loadError);
+    preload.loadManifest(manifest);
+}
+
+function handleFileProgress(event)
+{
+    console.log(preload.progress);
+}
+
+function loadComplete(event)
+{
+    console.log("Finished Loading Assets");
+    ReactDOM.render(<App />, document.getElementById('root'));
+    registerServiceWorker();
+}
+
+function loadError(event)
+{
+    console.log("Error!", event.text);
+    alert("Error");
+}
+
+setupManifest();
+startPreload();
 
