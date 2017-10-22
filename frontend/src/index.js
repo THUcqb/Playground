bc3446bed6ec7e95be045ebe261eda4d44500d94
左浩佳 <zuohj15@mails.tsinghua.edu.cn>
2017-10-23 00:50:21 +0800

    feat: Rendered texture for ground and walls.
    Resolving: #32

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,29 +6,40 @@ import createjs from 'preload-js';
 import registerServiceWorker from './registerServiceWorker';
 
 let manifest;
-let preload;
+const preloader = new createjs.LoadQueue(false);
 
 function setupManifest()
 {
     manifest = [{
-        src: "logo.svg",
-        id: "logo"
+        src: "http://www.hbwksj.com/uploads/allimg/160719/2-160G91P92c30.jpg",
+        id: "square"
+    }, {
+        src: "http://58pic.ooopic.com/58pic/12/91/84/91y58PIC2m7.jpg",
+        id: "wall"
+    }, {
+        src: "http://img2.3png.com/58671647c614592d044808852d898920d52a.png",
+        id: "coin"
     }
     ];
 }
 
 function startPreload()
 {
-    preload = new createjs.LoadQueue(false);
-    preload.on("progress", handleFileProgress);
-    preload.on("complete", loadComplete);
-    preload.on("error", loadError);
-    preload.loadManifest(manifest);
+    preloader.on("progress", handleFileProgress);
+    preloader.on("complete", loadComplete);
+    preloader.on("error", loadError);
+    preloader.loadManifest(manifest);
 }
 
 function handleFileProgress(event)
 {
-    console.log(preload.progress);
+    console.log(preloader.progress);
+}
+
+function loadError(event)
+{
+    console.log("Error!", event.text);
+    alert("Error");
 }
 
 function loadComplete(event)
@@ -38,12 +49,8 @@ function loadComplete(event)
     registerServiceWorker();
 }
 
-function loadError(event)
-{
-    console.log("Error!", event.text);
-    alert("Error");
-}
-
 setupManifest();
 startPreload();
+
+export {preloader};
 
