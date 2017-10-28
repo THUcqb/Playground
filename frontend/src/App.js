5634f470a2c571ec9b0d2ec3456c1270fb0b6e80
THUcqb <chenqibin422@gmail.com>
2017-10-28 19:56:47 +0800

    feat: add landing page at the beginning.
    Relate #45

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,14 @@ import Scene from './Scene';
 import Grid from 'material-ui/Grid';
 import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
 
 const styles = theme => ({
     root: {
         flexGrow: 1,
         overflow: "hidden",
         marginTop: 64,
-
     },
     container: {
     },
@@ -20,6 +21,17 @@ const styles = theme => ({
         textAlign: 'center',
         color: theme.palette.text.secondary,
     },
+    welcomepaper: {
+        textAlign: 'center',
+        padding: theme.spacing.unit * 20,
+        margin: theme.spacing.unit * 10,
+        marginTop: 0,
+        backgroundColor: theme.palette.background.contentFrame,
+    },
+    flex: {
+        color: theme.palette.text.secondary,
+        flex: 1,
+    }
 });
 
 /**
@@ -27,24 +39,68 @@ const styles = theme => ({
  */
 class App extends Component
 {
+    constructor(props) {
+        super(props);
+        this.state = {
+            welcome: true,
+        }
+    }
+
+    handleWelcome() {
+        this.setState({welcome: false});
+    }
+
     render() {
         const { classes } = this.props;
 
+        let body = null;
+        if (this.state.welcome === true)
+            body = (
+                <div>
+                    <Grid container className={classes.container}>
+                        <Grid item xs={12} sm={12}>
+                            <Paper className={classes.welcomepaper}>
+                                <Typography className={classes.flex} type="title">
+                                    FootMark is a boring game
+                                    <br/>
+                                    which helps you learn basic programming ideas!
+                                    <br/>
+                                    <br/>
+                                    Have a nice trip!
+                                    <br/>
+                                    <br/>
+                                </Typography>
+                                <Button raised color="primary" onClick={() => this.handleWelcome()}
+                                >
+                                    Have a try!
+                                </Button>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </div>
+            );
+        else
+            body = (
+                <div>
+                    <Navigation className={classes.appbar}/>
+                    <Grid container className={classes.container}>
+                        <Grid item xs={12} sm={6}>
+                            <Paper className={classes.paper}>
+                                <Scene/>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Paper className={classes.paper}>
+                                <Gamepad />
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </div>
+            );
+
         return (
             <div className={classes.root}>
-                <Navigation className={classes.appbar}/>
-                <Grid container className={classes.container}>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.paper}>
-                            <Scene/>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.paper}>
-                            <Gamepad />
-                        </Paper>
-                    </Grid>
-                </Grid>
+                {body}
             </div>
         );
     }
