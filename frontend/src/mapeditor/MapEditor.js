1859c7063e8171d5126334c9145abe6512639f56
闫力敏 <yanlm15@mails.tsinghua.edu.cn>
2017-11-01 15:40:10 +0800

    feat: add map size selection
    mention #48

--- frontend/src/mapeditor/MapEditor.js
+++ frontend/src/mapeditor/MapEditor.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
 import Dialog, {
     DialogTitle,
     DialogContent,
@@ -7,6 +8,7 @@ import Dialog, {
 } from 'material-ui/Dialog';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
+import MenuItem from 'material-ui/Menu/MenuItem';
 import Button from 'material-ui/Button';
 
 const styles = theme => ({
@@ -14,8 +16,32 @@ const styles = theme => ({
         display: 'flex',
         flexWrap: 'wrap',
     },
+    textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  
 });
 
+  const currencies = [
+  {
+    value: '0',
+    label: '0',
+  },
+  {
+    value: '10',
+    label: 'S',
+  },
+  {
+    value: '20',
+    label: 'L',
+  },
+  {
+    value: '15',
+    label: 'M',
+  },
+ ];
 /**
  * The MapEditor.
  */
@@ -33,11 +59,19 @@ class MapEditor extends React.Component {
     /**
      * Triggered when the user clicks SAVE button.
      */
+    
+    handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
     handleFinishEditing() {
     //TODO: to something including save the map and close the dialog.
 
         alert("map saved!");
     }
+
+
 
     render() {
         const { classes } = this.props;
@@ -52,9 +86,41 @@ class MapEditor extends React.Component {
                     <DialogContentText>
                         Explore and create your own map !
                     </DialogContentText>
-                    {/*TODO: Map Editor body here */}
+                    {/*TODO: Map Editor body here */
+                    <div>   
+                           
+                    <TextField
+                      id="select-size"
+                      select
+                      label="Size select"
+                      className={classes.textField}
+                      value={this.state.mapSize}
+                      onChange={this.handleChange('mapSize')}
+                      SelectProps={{
+                        native: true,
+                        MenuProps: {
+                          className: classes.menu,
+                        },
+                      }}
+                      helperText="Please select your Map size"
+                      margin="normal"
+                    >
+                      {currencies.map(option => (
+                        <option key={option.value} value={option.value}>
+                          {option.label}
+                        </option>
+                      ))}
+                    </TextField>
+                       <div>
+                          <canvas id="canvas" ref="canvas" width="400" height="400" />
+                       </div>
+                    </div>
+                    }
                 </DialogContent>
                 <DialogActions className={classes.actions}>
+                    <Button onClick={() => this.handleFinishEditing()} color="primary">
+                        Start
+                    </Button>
                     <Button onClick={() => this.handleFinishEditing()} color="primary">
                         Save
                     </Button>
@@ -69,3 +135,4 @@ MapEditor.propTypes = {
 };
 
 export default withStyles(styles, {withTheme: true})(MapEditor);
+
