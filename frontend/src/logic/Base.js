77156f1a9c35775127b2290633453c8a2e65bc68
左浩佳 <zuohj15@mails.tsinghua.edu.cn>
2017-11-06 14:13:29 +0800

    style: changed some tabs to spaces in Base.js
    Resolving: #59

--- frontend/src/logic/Base.js
+++ frontend/src/logic/Base.js
@@ -3,100 +3,100 @@ import {Base_state} from './Base_state';
 import Map from './Map';
 
 export class Base_task{
-	constructor(begin){
-		this.begin = begin;
-		this.tasklist = [];
-		this.size = 0;
-	}
-	/**
-	 * add a task in tasklist
-	 * @param {[Base]}  
-	 */
-	add(task){
-		this.tasklist[this.size] = task;
+    constructor(begin){
+        this.begin = begin;
+        this.tasklist = [];
+        this.size = 0;
+    }
+    /**
+     * add a task in tasklist
+     * @param {[Base]}
+     */
+    add(task){
+        this.tasklist[this.size] = task;
 
-		if (this.size > 0) {
-			this.tasklist[this.size-1].link(this.tasklist[this.size]);
-		}
-		this.tasklist[this.size].link(this.begin);
-		this.size+=1;
-	}
+        if (this.size > 0) {
+            this.tasklist[this.size-1].link(this.tasklist[this.size]);
+        }
+        this.tasklist[this.size].link(this.begin);
+        this.size+=1;
+    }
 }
 
 export class Base {
-	constructor(type,name,check) {
-		this.type = type;
-		this.name = name;
-		this.time = 1;
-		this.cur_time = 1;
-		this.check = check;
-	}
+    constructor(type,name,check) {
+        this.type = type;
+        this.name = name;
+        this.time = 1;
+        this.cur_time = 1;
+        this.check = check;
+    }
 
-	/**
-	 * link current task to next task
-	 * @param  {Function} next : naxt task
-	 */
-	link(next){
-		this.next = next;
-	}
-	/**
-	 * add tasklist for Base unit
-	 * @param {[Base_task]} tasklist 
-	 */
-	set_task(tasklist){
-		this.task = tasklist;
-	}
-	/**
-	 * set run time for tasklist
-	 * @param {[int]} time 
-	 */
-	set_time(time){
-		this.time = time;
-		this.cur_time = time;
-	}
-	/**
-	 * set else branch for Base unit 
-	 * "if...else do else_tasklist "
-	 * @param {[Base_task]} else_tasklist [description]
-	 */
-	set_else(else_tasklist){
-		this.else_task = else_tasklist;
-	}
-	/**
-	 * set check info "move" "check_move_up" ...
-	 * see also the function Check()
-	 * @param {[String]} check
-	 */
-	set_check(check){
-		this.check = check;
-	}
-	/**
-	 * chek point(x,y) if "runnable" or have "candy"
-	 */
-	runnable(x,y){	
-		if (x<0 || x>=10) {return'err';}
-		if (y<0 || y>=10) {return'err';}
-		if (Number(Base.bmap.block_list[x][y].info) === 2) {return'candy';}
-		if (Number(Base.bmap.block_list[x][y].info) === 0) {return'runnable';}
-		return 'err';
-	}
-	/**
-	 * Based on the check info check current state if right or not
-	 * runnable : the check info is right
-	 * null: current state is missing or wrong
-	 */
-	Check(str){
-		let s = "";
-		if (str === "true") {
-			return 'runnable'
-		}
-		else
-		if (str === "false") {
-		return 'null'
-		}
-		else
-		if (str === "check_move_up") {
-			let _x = Base.bsnake.x-1 ;
+    /**
+     * link current task to next task
+     * @param  {Function} next : naxt task
+     */
+    link(next){
+        this.next = next;
+    }
+    /**
+     * add tasklist for Base unit
+     * @param {[Base_task]} tasklist
+     */
+    set_task(tasklist){
+        this.task = tasklist;
+    }
+    /**
+     * set run time for tasklist
+     * @param {[int]} time
+     */
+    set_time(time){
+        this.time = time;
+        this.cur_time = time;
+    }
+    /**
+     * set else branch for Base unit
+     * "if...else do else_tasklist "
+     * @param {[Base_task]} else_tasklist [description]
+     */
+    set_else(else_tasklist){
+        this.else_task = else_tasklist;
+    }
+    /**
+     * set check info "move" "check_move_up" ...
+     * see also the function Check()
+     * @param {[String]} check
+     */
+    set_check(check){
+        this.check = check;
+    }
+    /**
+     * chek point(x,y) if "runnable" or have "candy"
+     */
+    runnable(x,y){
+        if (x<0 || x>=10) {return'err';}
+        if (y<0 || y>=10) {return'err';}
+        if (Number(Base.bmap.block_list[x][y].info) === 2) {return'candy';}
+        if (Number(Base.bmap.block_list[x][y].info) === 0) {return'runnable';}
+        return 'err';
+    }
+    /**
+     * Based on the check info check current state if right or not
+     * runnable : the check info is right
+     * null: current state is missing or wrong
+     */
+    Check(str){
+        let s = "";
+        if (str === "true") {
+            return 'runnable'
+        }
+        else
+        if (str === "false") {
+        return 'null'
+        }
+        else
+        if (str === "check_move_up") {
+            let _x = Base.bsnake.x-1 ;
 			let _y = Base.bsnake.y ;
 			s = this.runnable(_x,_y);
 		}
@@ -252,74 +252,74 @@ export class Base {
 		let _x = Base.bsnake.x ;
 		let _y = Base.bsnake.y+1;
 		this.base_move(_x,_y);
-	}
-	/**
-	 * Base unit will run it's tasklist automatically untill finshed 
-	 * Woring: if you want to move one step you'd better use next() in Base__state
-	 */
-	run(){
-		// console.log("run: > "+this.name)
-		if (this.type === "sys") {
-		if (this.name === "move_up"){
-			this.move_up();
-		}
-		else 
-		if (this.name === "move_down"){
-			this.move_down();
-		} 
-		else
-		if (this.name === "move_left"){
-			this.move_left();
-		} 
-		else
-		if (this.name === "move_right"){
-			this.move_right();
-		} 
-		// else
-		// if (this.name === "vmove_up"){} 
-		// else
-		// if (this.name === "vmove_down"){} 
-		// else
-		// if (this.name === "vmove_left"){}
-		// else 
-		// if (this.name === "vmove_right"){}
-		else
-		if (this.name === "loop"){
-			this.loop();
-		}
-		else
-		if (this.name === "do_while"){
-			this.do_while();
-		}
-		else
-		if (this.name === "while_do"){
-			this.while_do();
-		}
-		else
-		if (this.name === "judge") {
-			if (Base.Check(this.check) === "runnable") {
-				for (let variable in this.task.tasklist){ 
-					if (this.task.tasklist.hasOwnProperty(variable))
-					this.task.tasklist[variable].run()
-				}
-			}
-			else{
-				for (let variable in this.else_task.tasklist){ 
-					if (this.task.tasklist.hasOwnProperty(variable))
-					this.task.tasklist[variable].run()
-				}
-			}
-		}  
-			
-		} else 
-		if (this.type === "user") {
-			for (let variable in this.task.tasklist){ 
-				this.task.tasklist[variable].run()
-			}
-		}
-		
-		Base.bmap.print();
-	}
+    }
+    /**
+     * Base unit will run it's tasklist automatically untill finshed
+     * Woring: if you want to move one step you'd better use next() in Base__state
+     */
+    run(){
+        // console.log("run: > "+this.name)
+        if (this.type === "sys") {
+        if (this.name === "move_up"){
+            this.move_up();
+        }
+        else
+        if (this.name === "move_down"){
+            this.move_down();
+        }
+        else
+        if (this.name === "move_left"){
+            this.move_left();
+        }
+        else
+        if (this.name === "move_right"){
+            this.move_right();
+        }
+        // else
+        // if (this.name === "vmove_up"){}
+        // else
+        // if (this.name === "vmove_down"){}
+        // else
+        // if (this.name === "vmove_left"){}
+        // else
+        // if (this.name === "vmove_right"){}
+        else
+        if (this.name === "loop"){
+            this.loop();
+        }
+        else
+        if (this.name === "do_while"){
+            this.do_while();
+        }
+        else
+        if (this.name === "while_do"){
+            this.while_do();
+        }
+        else
+        if (this.name === "judge") {
+            if (Base.Check(this.check) === "runnable") {
+                for (let variable in this.task.tasklist){
+                    if (this.task.tasklist.hasOwnProperty(variable))
+                    this.task.tasklist[variable].run()
+                }
+            }
+            else{
+                for (let variable in this.else_task.tasklist){
+                    if (this.task.tasklist.hasOwnProperty(variable))
+                    this.task.tasklist[variable].run()
+                }
+            }
+        }
+
+        } else
+        if (this.type === "user") {
+            for (let variable in this.task.tasklist){
+                this.task.tasklist[variable].run()
+            }
+        }
+
+        Base.bmap.print();
+    }
 }
 
 Base.bmap = new Map(10,10); //初始化地图 每个地块的信息 Base.bmap.block_list[x][y].info 参考map.js
