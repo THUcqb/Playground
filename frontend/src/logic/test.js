88b105e2648b597500624100601d9605b2c77c14
左浩佳 <zuohj15@mails.tsinghua.edu.cn>
2017-11-07 08:37:13 +0800

    style: changed all tabs to spaces in the whole project
    Resolves: #59

--- frontend/src/logic/test.js
+++ frontend/src/logic/test.js
@@ -42,92 +42,92 @@ class test{
                     if (op === 's'|| op === 'S')
                     {
                         let b3 = new Base("sys","move_down","move");
-						b3.run();
-					}
-					if (op === 'a'|| op === 'A') 
-					{
-						let b2 = new Base("sys","move_left","move");
-						b2.run();
-					}
-					if (op === 'd'|| op === 'D') 
-					{
-						let b4 = new Base("sys","move_right","move");
-						b4.run();
-					}
-				  // this.runGame(loop);
-				});
-		}
-	}
-	testcontroller()
-	{
-		let op = ""
-		rl.on('line', (input) => {
-		console.log(`Received: ${input}`);
-		op = input;
-		console.log(">"+op);
-		controller.step();
-		});
-	}
+                        b3.run();
+                    }
+                    if (op === 'a'|| op === 'A') 
+                    {
+                        let b2 = new Base("sys","move_left","move");
+                        b2.run();
+                    }
+                    if (op === 'd'|| op === 'D') 
+                    {
+                        let b4 = new Base("sys","move_right","move");
+                        b4.run();
+                    }
+                  // this.runGame(loop);
+                });
+        }
+    }
+    testcontroller()
+    {
+        let op = ""
+        rl.on('line', (input) => {
+        console.log(`Received: ${input}`);
+        op = input;
+        console.log(">"+op);
+        controller.step();
+        });
+    }
 
-	testjudge()
-	{
-		let usertask = new Base_task(controller.begin)//将任务列表指向初始节点
-		let judge = new Base("sys","judge","move");
-			let true_task = new Base_task(judge);
-			let false_task = new Base_task(judge);
+    testjudge()
+    {
+        let usertask = new Base_task(controller.begin)//将任务列表指向初始节点
+        let judge = new Base("sys","judge","move");
+            let true_task = new Base_task(judge);
+            let false_task = new Base_task(judge);
 
-			true_task.add(new Base("sys","move"));
-			false_task.add(new Base("sys","trun_left"));
+            true_task.add(new Base("sys","move"));
+            false_task.add(new Base("sys","trun_left"));
 
-			judge.set_task(true_task);
-			judge.set_else(false_task);
+            judge.set_task(true_task);
+            judge.set_else(false_task);
 
-		let loop = new Base("sys","loop");
-			loop.set_time(200);
-			let loop_task = new Base_task(loop);
-			loop_task.add(judge);
-			loop.set_task(loop_task);
+        let loop = new Base("sys","loop");
+            loop.set_time(200);
+            let loop_task = new Base_task(loop);
+            loop_task.add(judge);
+            loop.set_task(loop_task);
 
-		usertask.add(loop);//向任务列表添加函数
-		controller.begin.task=usertask;
-		this.testcontroller()
-	}
-	testWhile()
-	{
-		let usertask = new Base_task(controller.begin)//将任务列表指向初始节点
-		let judge = new Base("sys","judge","move");
-			let true_task = new Base_task(judge);
-			let false_task = new Base_task(judge);
+        usertask.add(loop);//向任务列表添加函数
+        controller.begin.task=usertask;
+        this.testcontroller()
+    }
+    testWhile()
+    {
+        let usertask = new Base_task(controller.begin)//将任务列表指向初始节点
+        let judge = new Base("sys","judge","move");
+            let true_task = new Base_task(judge);
+            let false_task = new Base_task(judge);
 
-			true_task.add(new Base("sys","move"));
-			false_task.add(new Base("sys","trun_left"));
+            true_task.add(new Base("sys","move"));
+            false_task.add(new Base("sys","trun_left"));
 
-			judge.set_task(true_task);
-			judge.set_else(false_task);
+            judge.set_task(true_task);
+            judge.set_else(false_task);
 
-		let while_do = new Base("sys","while_do","true");
-			let while_do_task = new Base_task(while_do);
-			while_do_task.add(judge);
-			while_do.set_task(while_do_task);
+        let while_do = new Base("sys","while_do","true");
+            let while_do_task = new Base_task(while_do);
+            while_do_task.add(judge);
+            while_do.set_task(while_do_task);
 
-		usertask.add(while_do);//向任务列表添加函数
-		controller.begin.task=usertask;
-		this.testcontroller()
-	}
+        usertask.add(while_do);//向任务列表添加函数
+        controller.begin.task=usertask;
+        this.testcontroller()
+    }
 
-	testGameOver()
-	{
-		let usertask = new Base_task(controller.begin)//将任务列表指向初始节点
-		let loop = new Base("sys","loop");
-			loop.set_time(200);
-			let loop_task = new Base_task(loop);
-			loop_task.add(new Base("sys","move_up"));
-			loop.set_task(loop_task);
-			
-		usertask.add(loop);//向任务列表添加函数
-		controller.begin.task=usertask;
-		this.testcontroller()
-	}
+    testGameOver()
+    {
+        let usertask = new Base_task(controller.begin)//将任务列表指向初始节点
+        let loop = new Base("sys","loop");
+            loop.set_time(200);
+            let loop_task = new Base_task(loop);
+            loop_task.add(new Base("sys","move_up"));
+            loop.set_task(loop_task);
+            
+        usertask.add(loop);//向任务列表添加函数
+        controller.begin.task=usertask;
+        this.testcontroller()
+    }
 
 }
 
