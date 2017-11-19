9ff70b1b283f7465e4ee753cdf7c43051aa45d78
何杰 <hejie15@mails.tsinghua.edu.cn>
2017-11-19 22:21:41 +0800

    feat: implement update database.
    Resolved #113

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd backend
 coverage run --source . manage.py test
-coverage html -d ../Coverage_Python
+coverage html --omit=manage.py,api/apps.py,api/migrations/*,api/__init__.py,api/admin.py,api/urls.py,backend/*,savetoolbox.py -d ../Coverage_Python
